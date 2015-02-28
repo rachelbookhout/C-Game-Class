1d1cd5b303d4f30688b93ecab65e73f5ccf9d64e
rachelbookhout <rbookhou@gmail.com>
2015-02-28 17:39:57 -0500

    added lab 3

---
+++ lab3/lab3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace lab3
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+
+			float org_temp_in_f = 32;
+			float temp_in_c = (float)(org_temp_in_f - 32) / 9 * 5;
+			float fin_temp_in_f = temp_in_c * 9 / 5 + 32;
+				Console.WriteLine ( org_temp_in_f + " degrees Fahrenheit is" + temp_in_c + " degrees Celsius");
+			Console.WriteLine( temp_in_c + " degrees Celsius is" + fin_temp_in_f +  " degrees Fahrenheit");
+		}
+	}
+}
+
+
+
