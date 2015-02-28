65e753b437e83985910d2cb4a2635421c47fafd1
rachelbookhout <rbookhou@gmail.com>
2015-02-28 16:38:38 -0500

    completed lab 2

--- Lab2/Lab2/Program.cs
+++ Lab2/Lab2/Program.cs
@@ -4,9 +4,15 @@ namespace Lab2
 {
 	class MainClass
 	{
-		public static void Main (string[] args)
+		static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			int score= 88;
+			const int MAX_SCORE = 100;
+			float percent = score%MAX_SCORE;
+			
+			Console.WriteLine( percent + "%"); 
+
 		}
 	}
 }
+
