b5fb1f816ece8a01dcc45d02ac00f6ab629c17bf
rachelbookhout <rbookhou@gmail.com>
2015-03-01 13:49:30 -0500

    solved problem

--- ProgrammingAssignment1/ProgrammingAssignment1/Program.cs
+++ ProgrammingAssignment1/ProgrammingAssignment1/Program.cs
@@ -8,9 +8,17 @@ namespace ProgrammingAssignment1
 		{
 			Console.WriteLine ("This Application will calculate the max height of a shell and the distance it travels across the ground");
 			Console.WriteLine ("What angle did you launch the shell?");
-			float theta = float.Parse (Console.ReadLine ());
+			double theta =  Math.PI/ 180 * double.Parse (Console.ReadLine ());
 			Console.WriteLine("How fast did you launch the shell?");
-			float speed = float.Parse (Console.ReadLine ());
+			double speed = double.Parse (Console.ReadLine ());
+			double vox = speed * Math.Cos (theta); 
+			double voy = speed * Math.Sin (theta);
+			double g = 9.8;
+			double t = voy / g;
+			double height = voy * voy / (2 * g);
+			double dx = vox * 2 * t;    
+			Console.WriteLine("Height of Shell at apex is:" + Math.Round(height,2) +". Distance Shell Traveled is:" + Math.Round(dx,2));
+
 		}
 	}
 }
