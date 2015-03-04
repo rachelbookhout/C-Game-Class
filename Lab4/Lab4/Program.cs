5d030d33797d22e52ed66fd95afd520162afe75f
rachelbookhout <rbookhou@gmail.com>
2015-03-03 20:52:27 -0500

    started setup of class

---
+++ Lab4/Lab4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab4
+{
+	/// <summary>
+	/// Demonstrates classes and objects
+	/// </summary>
+	class Program
+	{
+		/// <summary>
+		/// Demonstrates use of Deck and Card objects
+		/// </summary>
+		/// <param name="args">command-line args</param>
+		static void Main(string[] args)
+		{
+			// create a new deck and print the contents of the deck
+
+			// shuffle the deck and print the contents of the deck
+
+			// take the top card from the deck and print the card rank and suit
+
+			// take the top card from the deck and print the card rank and suit
+
+
+		}
+	}
+}
