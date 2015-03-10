27f363564ea599bb4c77dc5e415ce672dab05f8a
rachelbookhout <rbookhou@gmail.com>
2015-03-09 20:58:51 -0400

    done

--- Lab7/Lab7/Program.cs
+++ Lab7/Lab7/Program.cs
@@ -6,7 +6,11 @@ namespace Lab7
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("");
+			Console.Write ("What month were you born?");
+			string birthmonth = Console.ReadLine ();
+			Console.Write("What day were you born?");
+			int birthdate = int.Parse (Console.ReadLine ());
+			Console.Write ("You were born " + birthmonth + " " + birthdate );
 		}
 	}
 }
