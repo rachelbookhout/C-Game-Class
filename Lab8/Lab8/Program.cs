1c67998aa914213bad3ae203526602ac89355b40
rachelbookhout <rbookhou@gmail.com>
2015-03-10 22:29:23 -0400

    debugged

--- Lab8/Lab8/Program.cs
+++ Lab8/Lab8/Program.cs
@@ -14,18 +14,21 @@ namespace Lab8
 			Console.WriteLine ("Tell us your slot number, block letter, and if it should be lit  (number,letter, true or false)");
 			//extract the pyramid slot number from the string and store it in a variable.
 			//Print the pyramid slot number.
-			answer = Console.ReadLine ();
-			int slot = int.Parse ();
+			string answer = Console.ReadLine ();
+			int commaLocation = answer.IndexOf(',');
+			float slot = float.Parse (answer.Substring(0,commaLocation));
 			Console.WriteLine ("Slot number is " + slot);
 			//Extract the block letter from the string and store it in a variable.
 			//Print the block letter.
-			char letter = 
-			Console.WriteLine ("Block letter is " + letter);
+			string second_string = answer.Substring (commaLocation + 1);
+				Console.Write(second_string);
+			//char letter = 
+			//Console.WriteLine ("Block letter is " + letter);
 
 			//Extract whether or not the block should be lit from the string and store it in a variable.
 			//Print whether or not the block should be lit.
-			string lit = 
-			Console.WriteLine ("Block is lit: " + lit);
+			//string lit = 
+			//Console.WriteLine ("Block is lit: " + lit);
 		}
 	}
 }
