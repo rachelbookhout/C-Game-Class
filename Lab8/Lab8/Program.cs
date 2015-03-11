5498186852487ed49a91d670a0c57a3cb3d2fa0b
rachelbookhout <rbookhou@gmail.com>
2015-03-10 22:00:47 -0400

    setup of lab8

---
+++ Lab8/Lab8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Lab8
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+
+			//Read in a string from the user in the following format:
+			//<pyramid slot number>,<block letter>,<whether or not the block should be lit>
+			//Example:
+			//15,M,true 
+			Console.WriteLine ("Tell us your slot number, block letter, and if it should be lit  (number,letter, true or false)");
+			//extract the pyramid slot number from the string and store it in a variable.
+			//Print the pyramid slot number.
+			answer = Console.ReadLine ();
+			int slot = int.Parse ();
+			Console.WriteLine ("Slot number is " + slot);
+			//Extract the block letter from the string and store it in a variable.
+			//Print the block letter.
+			char letter = 
+			Console.WriteLine ("Block letter is " + letter);
+
+			//Extract whether or not the block should be lit from the string and store it in a variable.
+			//Print whether or not the block should be lit.
+			string lit = 
+			Console.WriteLine ("Block is lit: " + lit);
+		}
+	}
+}
