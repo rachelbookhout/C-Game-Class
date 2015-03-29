823993f53ccc869f1f1c184ce453c4b16ca3c3e0
rachelbookhout <rbookhou@gmail.com>
2015-03-29 16:33:21 -0400

    added assignment 4

---
+++ ProgrammingAssignment4/ProgrammingAssignment4/Game1.cs
@@ -0,0 +1,165 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace ProgrammingAssignment4
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        const int WINDOW_WIDTH = 800;
+        const int WINDOW_HEIGHT = 600;
+
+        // teddy support
+        Texture2D teddySprite;
+        Teddy teddy;
+
+        // pickup support
+        Texture2D pickupSprite;
+        List<Pickup> pickups = new List<Pickup>();
+
+        // click processing
+        bool rightClickStarted = false;
+        bool rightButtonReleased = true;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            // STUDENTS: set resolution and make mouse visible
+
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // STUDENTS: load teddy and pickup sprites
+
+
+            // STUDENTS: create teddy object centered in window
+
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            // STUDENTS: get current mouse state and update teddy
+            MouseState mouse = Mouse.GetState();
+
+            // check for right click started
+            if (mouse.RightButton == ButtonState.Pressed &&
+                rightButtonReleased)
+            {
+                rightClickStarted = true;
+                rightButtonReleased = false;
+            }
+            else if (mouse.RightButton == ButtonState.Released)
+            {
+                rightButtonReleased = true;
+
+                // if right click finished, add new pickup to list
+                if (rightClickStarted)
+                {
+                    rightClickStarted = false;
+
+                    // STUDENTS: add a new pickup to the end of the list of pickups
+
+
+                    // STUDENTS: if this is the first pickup in the list, set teddy target
+
+                }
+            }
+
+            // STUDENTS: Delete the line saying if (true) and uncomment the three
+            // lines below that AFTER you've created a teddy object in the
+            // LoadContent method
+            // check for collision between collecting teddy and targeted pickup
+            if (true)
+            //if (teddy.Collecting &&
+            //     pickups.Count > 0 &&
+            //     teddy.CollisionRectangle.Intersects(pickups[0].CollisionRectangle))
+            {
+                // STUDENTS: remove targeted pickup from list (it's always at location 0)
+
+
+                // STUDENTS: if there's another pickup to collect, set teddy target
+                // If not, stop the teddy from collecting
+
+            }
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // draw game objects
+            spriteBatch.Begin();
+
+            // STUDENTS: Uncomment the following line AFTER you create
+            // a teddy object in the LoadContent method
+            //teddy.Draw(spriteBatch);
+            foreach (Pickup pickup in pickups)
+            {
+                pickup.Draw(spriteBatch);
+            }
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ ProgrammingAssignment4/ProgrammingAssignment4/Teddy.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ProgrammingAssignment4
+{
+    /// <summary>
+    /// A teddy bear
+    /// </summary>
+    public class Teddy
+    {
+        #region Fields
+
+        bool collecting = false;
+
+        // drawing support
+        Texture2D sprite;
+        Rectangle drawRectangle;
+        int halfDrawRectangleWidth;
+        int halfDrawRectangleHeight;
+
+        // moving support
+        const float BASE_SPEED = 0.3f;
+        Vector2 location;
+        Vector2 velocity = Vector2.Zero;
+
+        // click processing
+        bool leftClickStarted = false;
+        bool leftButtonReleased = true;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="sprite">sprite for the teddy</param>
+        /// <param name="location">location of the center of the teddy</param>
+        public Teddy(Texture2D sprite, Vector2 location)
+        {
+            this.sprite = sprite;
+            this.location = location;
+
+            // STUDENTS: set draw rectangle so teddy is centered on location
+
+
+            // STUDENTS: set halfDrawRectangleWidth and halfDrawRectangleHeight for efficiency
+
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets and sets whether or not the teddy is collecting
+        /// </summary>
+        public bool Collecting
+        {
+            get { return collecting; }
+            set { collecting = value; }
+        }
+
+        /// <summary>
+        /// Gets the collision rectangle for the teddy
+        /// </summary>
+        public Rectangle CollisionRectangle
+        {
+            get { return drawRectangle; }
+        }
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Updates the teddy
+        /// </summary>
+        /// <param name="gameTime">game time</param>
+        /// <param name="mouse">current mouse state</param>
+        public void Update(GameTime gameTime, MouseState mouse)
+        {
+            // STUDENTS: update location based on velocity if teddy is collecting
+            // You should update the location vector (which is at the center of
+            // the teddy) first using the velocity vector and the elapsed game time,
+            // then update the draw rectangle properties so the teddy is drawn
+            // centered on the location vector
+            // This gives us accurate movement toward the target so we don't miss
+            // the target due to rounding error
+
+
+            // check for mouse over teddy
+            if (drawRectangle.Contains(mouse.X, mouse.Y))
+            {
+                // check for left click started on teddy
+                if (mouse.LeftButton == ButtonState.Pressed &&
+                    leftButtonReleased)
+                {
+                    leftClickStarted = true;
+                    leftButtonReleased = false;
+                }
+                else if (mouse.LeftButton == ButtonState.Released)
+                {
+                    leftButtonReleased = true;
+
+                    // if click finished on teddy, start collecting
+                    if (leftClickStarted)
+                    {
+                        collecting = true;
+                        leftClickStarted = false;
+                    }
+                }
+            }
+            else
+            {
+                // no clicking on teddy
+                leftClickStarted = false;
+                leftButtonReleased = false;
+            }
+        }
+
+        /// <summary>
+        /// Draws the teddy
+        /// </summary>
+        /// <param name="spriteBatch">sprite batch</param>
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            // STUDENTS: use the sprite batch to draw the teddy
+
+        }
+
+        /// <summary>
+        /// Sets a target for the teddy to move toward
+        /// </summary>
+        /// <param name="target">target</param>
+        public void SetTarget(Vector2 target)
+        {
+            // STUDENTS: set teddy velocity based on teddy center location and target
+            // One way to do this is to create a new vector that consists of the
+            // distances in x and y between the target and the teddy. Make sure to
+            // normalize the vector, then multiply the vector by the teddy's base
+            // speed
+
+        }
+
+        #endregion
+    }
+}
