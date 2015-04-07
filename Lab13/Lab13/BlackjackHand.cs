4c4d71e9725bbdd233b4866d91592722993fbf1a
rachelbookhout <rbookhou@gmail.com>
2015-04-06 21:06:08 -0400

    created lab13

---
+++ Lab13/Lab13/BlackjackHand.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleCards
+{
+    /// <summary>
+    /// A class for a Blackjack hand
+    /// </summary>
+    public class BlackjackHand
+    {
+        #region Fields
+
+        const int MAX_HAND_VALUE = 21;
+
+        string ownerName;
+        List<Card> cards = new List<Card>();
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="ownerName">the name of the player owning the hand</param>
+        public BlackjackHand(string ownerName)
+        {
+            this.ownerName = ownerName;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the score for the hand
+        /// </summary>
+        public int Score
+        {
+            get
+            {
+                // add up score excluding Aces
+                int numAces = 0;
+                int score = 0;
+                foreach (Card card in cards)
+                {
+                    if (card.Rank != Rank.Ace.ToString())
+                    {
+                        score += card.BlackjackValue;
+                    }
+                    else
+                    {
+                        numAces++;
+                    }
+                }
+
+                // if more than one ace, only one should ever be counted as 11
+                if (numAces > 1)
+                {
+                    // make all but the first ace count as 1
+                    score += numAces - 1;
+                    numAces = 1;
+                }
+
+                // if there's an Ace,score it the best way possible
+                if (numAces > 0)
+                {
+                    if (score + 11 <= MAX_HAND_VALUE)
+                    {
+                        // counting Ace as 11 doesn't bust
+                        score += 11;
+                    }
+                    else
+                    {
+                        // count Ace as 1
+                        score++;
+                    }
+                }
+
+                return score;
+            }
+        }
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Adds a card to the hand
+        /// </summary>
+        /// <param name="card">the card to add</param>
+        public void AddCard(Card card)
+        {
+            cards.Add(card);
+        }
+
+        /// <summary>
+        /// Lets a player decide whether or not to hit using console input
+        /// </summary>
+        /// <param name="deck">the deck to potentially hit from</param>
+        public void HitOrNot(Deck deck)
+        {
+            if (Hit())
+            {
+                Card card = deck.TakeTopCard();
+                if (!card.FaceUp)
+                {
+                    card.FlipOver();
+                }
+                Console.Write("Your new card is the ");
+                card.Print();
+                Console.WriteLine();
+                cards.Add(card);
+            }
+        }
+
+        /// <summary>
+        /// Shows the first card in the hand by making it face up
+        /// </summary>
+        public void ShowFirstCard()
+        {
+            if (cards.Count > 0 &&
+                !cards[0].FaceUp)
+            {
+                cards[0].FlipOver();
+            }
+        }
+
+        /// <summary>
+        /// Shows all the cards in the hand by making them face up
+        /// </summary>
+        public void ShowAllCards()
+        {
+            foreach (Card card in cards)
+            {
+                if (!card.FaceUp)
+                {
+                    card.FlipOver();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Prints the cards in the hand
+        /// </summary>
+        public void Print()
+        {
+            string headerName = ownerName + "'s Hand";
+            Console.WriteLine(headerName);
+            Console.WriteLine(GetDashesString(headerName.Length));
+            foreach (Card card in cards)
+            {
+                card.Print();
+            }
+            Console.WriteLine();
+        }
+
+        #endregion
+
+        #region Private methods
+
+        /// <summary>
+        /// Gets a string of dashes of the given length
+        /// </summary>
+        /// <param name="length">the length of the string</param>
+        /// <returns>the string of dashes</returns>
+        private string GetDashesString(int length)
+        {
+            StringBuilder dashes = new StringBuilder();
+            for (int i = 0; i < length; i++)
+            {
+                dashes.Append('-');
+            }
+            return dashes.ToString();
+        }
+
+        /// <summary>
+        /// Tells whether or not the player wants to hit
+        /// </summary>
+        /// <returns>whether to hit or not</returns>
+        private bool Hit()
+        {
+            char hitChar = 'z';
+
+            // prompt for and get hit or not decision
+            bool valid = false;
+            while (!valid)
+            {
+                // prompt for and get player choice
+                Console.Write("Would you like to hit (y, n)? ");
+                string choice = Console.ReadLine();
+
+                // print error message as necessary
+                if (choice.Length > 0)
+                {
+                    hitChar = Char.ToLower(choice[0]);
+                }
+                else
+                {
+                    hitChar = 'z';
+                }
+                if (choice.Length > 1 ||
+                    (hitChar != 'y' && hitChar != 'n'))
+                {
+                    hitChar = 'z';
+                    Console.WriteLine("You need to enter a single character, y for yes or n for no!");
+                    Console.WriteLine();
+                }
+                valid = hitChar == 'y' || hitChar == 'n';
+            }
+
+            Console.WriteLine();
+            return hitChar == 'y';
+        }
+
+        #endregion
+    }
+}
---
+++ Lab13/Lab13/Properties/Card.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleCards
+{
+    /// <summary>
+    /// A class for a playing card
+    /// </summary>
+    public class Card
+    {
+        #region Fields
+
+        Rank rank;
+        Suit suit;
+        bool faceUp;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Constructs a card with the given rank and suit
+        /// </summary>
+        /// <param name="rank">the rank</param>
+        /// <param name="suit">the suit</param>
+        public Card(Rank rank, Suit suit)
+        {
+            this.rank = rank;
+            this.suit = suit;
+            faceUp = false;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the card rank
+        /// </summary>
+        public string Rank
+        {
+            get { return rank.ToString(); }
+        }
+
+        /// <summary>
+        /// Gets the card suit
+        /// </summary>
+        public string Suit
+        {
+            get { return suit.ToString(); }
+        }
+
+        /// <summary>
+        /// Gets whether or not the card is face up
+        /// </summary>
+        public bool FaceUp
+        {
+            get { return faceUp; }
+        }
+
+        /// <summary>
+        /// Gets the Blackjack value for the card
+        /// </summary>
+        public int BlackjackValue
+        {
+            get
+            {
+                switch (rank)
+                {
+                    case ConsoleCards.Rank.Ace:
+                        return 11;
+                    case ConsoleCards.Rank.King:
+                    case ConsoleCards.Rank.Queen:
+                    case ConsoleCards.Rank.Jack:
+                    case ConsoleCards.Rank.Ten:
+                        return 10;
+                    case ConsoleCards.Rank.Nine:
+                        return 9;
+                    case ConsoleCards.Rank.Eight:
+                        return 8;
+                    case ConsoleCards.Rank.Seven:
+                        return 7;
+                    case ConsoleCards.Rank.Six:
+                        return 6;
+                    case ConsoleCards.Rank.Five:
+                        return 5;
+                    case ConsoleCards.Rank.Four:
+                        return 4;
+                    case ConsoleCards.Rank.Three:
+                        return 3;
+                    case ConsoleCards.Rank.Two:
+                        return 2;
+                    default:
+                        return 0;
+                }
+            }
+        }
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Flips the card over
+        /// </summary>
+        public void FlipOver()
+        {
+            faceUp = !faceUp;
+        }
+
+        /// <summary>
+        /// Prints the card
+        /// </summary>
+        public void Print()
+        {
+            if (FaceUp)
+            {
+                Console.WriteLine(Rank + " of " + Suit);
+            }
+            else
+            {
+                Console.WriteLine("Face down card");
+            }
+        }
+
+        #endregion
+    }
+}
