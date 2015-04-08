1e1f59eecb153ee5bfc5106aeb4ece68a1d35c09
rachelbookhout <rbookhou@gmail.com>
2015-04-07 21:03:49 -0400

    added lab13a

---
+++ Lab13a/Lab13a/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lab13a
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			
+
+					for (int i = 1; i <= 5; i++)
+					{
+						Console.WriteLine(i);
+					}
+
+			for (int i = 5; 0 < i; i--)
+			{
+				Console.WriteLine(i);
+			}
+		}
+	}
+}
