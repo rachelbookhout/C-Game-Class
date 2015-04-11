63101eaa508ac692ae3d2f02f54607006336fa78
rachelbookhout <rbookhou@gmail.com>
2015-04-11 16:52:36 -0400

    lab 14

--- Lab14/Lab14/Program.cs
+++ Lab14/Lab14/Program.cs
@@ -1,14 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace Lab14
 {
-	class MainClass
+	class Program
 	{
-		public static void Main (string[] args)
+		static void Main(string[] args)
 		{
-			Console.WriteLine ("Pick a number");
-			float choice = Console.ReadLine ();
+			// define a list of inputs
+			List<int> inputs = new List<int>();
 
+			int number = 0; // Initialize
+			while (number != -1) // Test
+			{
+				Console.WriteLine("Enter a non-negative number (or -1 to end the list): ");
+				number = int.Parse(Console.ReadLine());  // Modify
+				if (number >= 0) inputs.Add(number);  // number is valid so add number to list
+				else if (number == -1) {;} // this will exit from the loop
+				else Console.WriteLine("Not a negative number");
+			}
+
+			int sum = 0;
+			int max = 0; // all numbers must be non-negative
+			Console.WriteLine("\nNumbers Entered");
+			foreach (int item in inputs)
+			{
+				Console.WriteLine(item);
+				sum += item;
+				if (item > max) max = item;
+			}
+			Console.WriteLine("\nMaximum:  " + max);
+
+			double average = ((double) sum) / ((double) inputs.Count);
+			Console.WriteLine("Average:  " + average);
+
+			Console.ReadLine();
 		}
 	}
 }
