65d7a32b76560b8706a1573c2aee74712946bd80
rachelbookhout <rbookhou@gmail.com>
2015-04-20 18:28:23 -0400

    added setup for blackjack

--- ProgrammingAssisgnment6/ProgrammingAssisgnment6/Game1.cs
+++ ProgrammingAssisgnment6/ProgrammingAssisgnment6/Game1.cs
@@ -1,112 +1,279 @@
-﻿#region File Description
-//-----------------------------------------------------------------------------
-// ProgrammingAssisgnment6Game.cs
-//
-// Microsoft XNA Community Game Platform
-// Copyright (C) Microsoft Corporation. All rights reserved.
-//-----------------------------------------------------------------------------
-#endregion
-
-#region Using Statements
 using System;
-
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Input.Touch;
-using Microsoft.Xna.Framework.Storage;
-using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Media;
 
-#endregion
+using XnaCards;
 
-namespace ProgrammingAssisgnment6
+namespace ProgrammingAssignment6
 {
-	/// <summary>
-	/// Default Project Template
-	/// </summary>
-	public class Game1 : Game
-	{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
 
-		#region Fields
+        const int WINDOW_WIDTH = 800;
+        const int WINDOW_HEIGHT = 600;
 
-		GraphicsDeviceManager graphics;
-		SpriteBatch spriteBatch;
-		Texture2D logoTexture;
+        // max valid blackjack score for a hand
+        const int MAX_HAND_VALUE = 21;
 
-		#endregion
+        // deck and hands
+        Deck deck;
+        List<Card> dealerHand = new List<Card>();
+        List<Card> playerHand = new List<Card>();
 
-		#region Initialization
+        // hand placement
+        const int TOP_CARD_OFFSET = 100;
+        const int HORIZONTAL_CARD_OFFSET = 150;
+        const int VERTICAL_CARD_SPACING = 125;
 
-		public Game1 ()
-		{
+        // messages
+        SpriteFont messageFont;
+        const string SCORE_MESSAGE_PREFIX = "Score: ";
+        Message playerScoreMessage;
+        List<Message> messages = new List<Message>();
 
-			graphics = new GraphicsDeviceManager (this);
-			
-			Content.RootDirectory = "Assets";
+        // message placement
+        const int SCORE_MESSAGE_TOP_OFFSET = 25;
+        const int HORIZONTAL_MESSAGE_OFFSET = HORIZONTAL_CARD_OFFSET;
+        Vector2 winnerMessageLocation = new Vector2(WINDOW_WIDTH / 2,
+            WINDOW_HEIGHT / 2);
 
-			graphics.IsFullScreen = false;
-		}
+        // menu buttons
+        Texture2D quitButtonSprite;
+        List<MenuButton> menuButtons = new List<MenuButton>();
 
-		/// <summary>
-		/// Overridden from the base Game.Initialize. Once the GraphicsDevice is setup,
-		/// we'll use the viewport to initialize some values.
-		/// </summary>
-		protected override void Initialize ()
-		{
-			base.Initialize ();
-		}
+        // menu button placement
+        const int TOP_MENU_BUTTON_OFFSET = TOP_CARD_OFFSET;
+        const int QUIT_MENU_BUTTON_OFFSET = WINDOW_HEIGHT - TOP_CARD_OFFSET;
+        const int HORIZONTAL_MENU_BUTTON_OFFSET = WINDOW_WIDTH / 2;
+        const int VERTICAL_MENU_BUTTON_SPACING = 125;
+
+        // use to detect hand over when player and dealer didn't hit
+        bool playerHit = false;
+        bool dealerHit = false;
+
+        // game state tracking
+        static GameState currentState = GameState.WaitingForPlayer;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            // set resolution and show mouse
+
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // create and shuffle deck
 
 
-		/// <summary>
-		/// Load your graphics content.
-		/// </summary>
-		protected override void LoadContent ()
-		{
-			// Create a new SpriteBatch, which can be use to draw textures.
-			spriteBatch = new SpriteBatch (graphics.GraphicsDevice);
-			
-			// TODO: use this.Content to load your game content here eg.
-			logoTexture = Content.Load<Texture2D> ("logo");
-		}
+            // first player card
 
-		#endregion
 
-		#region Update and Draw
+            // first dealer card
 
-		/// <summary>
-		/// Allows the game to run logic such as updating the world,
-		/// checking for collisions, gathering input, and playing audio.
-		/// </summary>
-		/// <param name="gameTime">Provides a snapshot of timing values.</param>
-		protected override void Update (GameTime gameTime)
-		{
-			// TODO: Add your update logic here			
-            		
-			base.Update (gameTime);
-		}
 
-		/// <summary>
-		/// This is called when the game should draw itself. 
-		/// </summary>
-		/// <param name="gameTime">Provides a snapshot of timing values.</param>
-		protected override void Draw (GameTime gameTime)
-		{
-			// Clear the backbuffer
-			graphics.GraphicsDevice.Clear (Color.CornflowerBlue);
+            // second player card
 
-			spriteBatch.Begin ();
 
-			// draw the logo
-			spriteBatch.Draw (logoTexture, new Vector2 (130, 200), Color.White);
+            // second dealer card
 
-			spriteBatch.End ();
 
-			//TODO: Add your drawing code here
-			base.Draw (gameTime);
-		}
+            // load sprite font, create message for player score and add to list
+            messageFont = Content.Load<SpriteFont>("Arial24");
+            playerScoreMessage = new Message(SCORE_MESSAGE_PREFIX + GetBlackjackScore(playerHand).ToString(),
+                messageFont,
+                new Vector2(HORIZONTAL_MESSAGE_OFFSET, SCORE_MESSAGE_TOP_OFFSET));
+            messages.Add(playerScoreMessage);
 
-		#endregion
-	}
+            // load quit button sprite for later use
+            quitButtonSprite = Content.Load<Texture2D>("quitbutton");
+
+            // create hit button and add to list
+
+
+            // create stand button and add to list
+
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            // update menu buttons as appropriate
+
+
+            // game state-specific processing
+
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Goldenrod);
+
+            spriteBatch.Begin();
+
+            // draw hands
+
+
+            // draw messages
+
+
+            // draw menu buttons
+
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+        /// <summary>
+        /// Calculates the Blackjack score for the given hand
+        /// </summary>
+        /// <param name="hand">the hand</param>
+        /// <returns>the Blackjack score for the hand</returns>
+        private int GetBlackjackScore(List<Card> hand)
+        {
+            // add up score excluding Aces
+            int numAces = 0;
+            int score = 0;
+            foreach (Card card in hand)
+            {
+                if (card.Rank != Rank.Ace)
+                {
+                    score += GetBlackjackCardValue(card);
+                }
+                else
+                {
+                    numAces++;
+                }
+            }
+
+            // if more than one ace, only one should ever be counted as 11
+            if (numAces > 1)
+            {
+                // make all but the first ace count as 1
+                score += numAces - 1;
+                numAces = 1;
+            }
+
+            // if there's an Ace, score it the best way possible
+            if (numAces > 0)
+            {
+                if (score + 11 <= MAX_HAND_VALUE)
+                {
+                    // counting Ace as 11 doesn't bust
+                    score += 11;
+                }
+                else
+                {
+                    // count Ace as 1
+                    score++;
+                }
+            }
+
+            return score;
+        }
+
+        /// <summary>
+        /// Gets the Blackjack value for the given card
+        /// </summary>
+        /// <param name="card">the card</param>
+        /// <returns>the Blackjack value for the card</returns>
+        private int GetBlackjackCardValue(Card card)
+        {
+            switch (card.Rank)
+            {
+                case Rank.Ace:
+                    return 11;
+                case Rank.King:
+                case Rank.Queen:
+                case Rank.Jack:
+                case Rank.Ten:
+                    return 10;
+                case Rank.Nine:
+                    return 9;
+                case Rank.Eight:
+                    return 8;
+                case Rank.Seven:
+                    return 7;
+                case Rank.Six:
+                    return 6;
+                case Rank.Five:
+                    return 5;
+                case Rank.Four:
+                    return 4;
+                case Rank.Three:
+                    return 3;
+                case Rank.Two:
+                    return 2;
+                default:
+                    return 0;
+            }
+        }
+
+        /// <summary>
+        /// Changes the state of the game
+        /// </summary>
+        /// <param name="newState">the new game state</param>
+        public static void ChangeState(GameState newState)
+        {
+            currentState = newState;
+        }
+    }
 }
