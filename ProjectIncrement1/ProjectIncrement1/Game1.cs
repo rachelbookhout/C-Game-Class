de3d68f332dbdb2d3870b74b76c3dc99733dfb4e
rachelbookhout <rbookhou@gmail.com>
2015-04-29 09:59:11 -0400

    edited spawn bear

--- ProjectIncrement1/ProjectIncrement1/Game1.cs
+++ ProjectIncrement1/ProjectIncrement1/Game1.cs
@@ -312,7 +312,11 @@ namespace GameProject
 			// create new bear
 			TeddyBear newBear = new TeddyBear (Content,"teddybear",xlocation, ylocation,vec,null,null);
 			// make sure we don't spawn into a collision
-
+			GetCollisionRectangles();
+			while (!CollisionUtils.IsCollisionFree)
+			{
+				
+			}
 			// add new bear to list
 			bears.Add (newBear);
 		}
